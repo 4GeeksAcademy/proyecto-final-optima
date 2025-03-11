d8fb3f5821256106e17c9ece85866af496a8fdf1
Camilo Cortes <100862481+camilocortes27@users.noreply.github.com>
2025-03-11 10:03:57 +0000

    creacion de nueva cardDetails

---
+++ src/front/js/component/cardDetails.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { ModalDetails } from "./modalDetails";
+
+export const CardDetails = (props) => {
+    const [showBalance, setShowBalance] = useState(true)
+    const path = useLocation()
+
+    const toggleBalance = () => {
+        let toggle = !showBalance
+        setShowBalance(toggle);
+    }
+    return (
+        <>
+            <div className="card flex-row mb-3 " key={props.id} >
+                <div className="card-body d-flex justify-content-around col-4 align-items-center " >
+                    <div className="justify-content-center">
+                        <h5 className="card-title ">Detalle Movimiento</h5>
+                        <div className="">
+                            <p>{props.detail}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-body d-flex justify-content-around col-4 align-items-center">
+                    <div className="justify-content-center">
+                        <p>{props.amount}</p>
+                        <p>{props.coin}</p>
+                    </div>
+                </div>
+                <div className="card-body d-flex justify-content-around col-4 align-items-center ">
+                    <p>{props.date}</p>
+                    <p>{props.time}</p>
+                    <p>{props.type}</p>
+                    <p>{props.operation}</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/front/js/pages/principalPage.js
+++ src/front/js/pages/principalPage.js
@@ -8,6 +8,7 @@ import { Link, useNavigate, useLocation, useParams } from "react-router-dom";
 import { Modal } from "../component/modal";
 import { ModalDetails } from "../component/modalDetails";
 import { CardMovimientos } from "../component/cardMovimiento";
+import { CardDetails } from "../component/cardDetails";
 
 
 export const PrincipalPage = () => {
@@ -61,16 +62,33 @@ export const PrincipalPage = () => {
                             ) : (
                                 <p>No hay cuentas disponibles.</p>
                             )
-                        ) : (
+                        ) : path.pathname.startsWith("/cuentas/") ? (
                             store.detailAccounts.length > 0 ? (
                                 store.detailAccounts.map((details) => (
                                     <CardMovimientos key={details.id} amount={details.amount} coin={details.coin} date={details.date} time={details.time} detail={details.detail} type={details.type} operation={details.operation} />
                                 ))
                             ) : (
-                                <p>No hay cuentas disponibles.</p>
+                                <p>No hay movimientos en esta cuenta.</p>
                             )
-                        )}
+                        ) : path.pathname === "/movimientos" ? (
+                            <>
+                                {console.log("Contenido de store.detailUser:", store.detailUser)}
+                                {store.detailUser.length > 0 ? (
+                                    store.detailUser.map((movents) => {
+                                        console.log("Movimientos de todas las cuentas:", movents);
+                                        return (
+                                            <CardDetails key={movents.id} amount={movents.amount} coin={movents.coin} date={movents.date} time={movents.time} detail={movents.detail} type={movents.type} operation={movents.operation} />
+                                        );
+                                    })
+                                ) : (
+                                    <p>No hay movimientos en ninguna cuenta.</p>
+                                )}
+                            </>
+                        ) : null}
                     </div>
+
+
+
                 </div>
                 {path.pathname === "/cuentas" ? <Modal /> : <ModalDetails />}
             </div>
